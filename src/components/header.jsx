0ce41a76f43c6aa6945cc493dc8ac636795bb83b
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-07-19 21:45:43 +0300

    working with the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,25 @@ import logo from "../assets/img/honey.png";
 const Header = () => {
   return (
     <header className="header">
-      <img src={logo} alt="Logo" />
+      <div className="header__container">
+        <div ckassName="header__all">
+          <img src={logo} alt="Logo" />
+          <div className="header__nav">
+            <a href="Home" className="header__link">
+              Home
+            </a>
+            <a href="About" className="header__link">
+              About
+            </a>
+            <a href="Our Product" className="header__link">
+              Our Product
+            </a>
+            <a href="Contact" className="header__link">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
