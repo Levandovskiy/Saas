0903dabb8dc478847b6158f03be74a494d29a3e1
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-08-05 23:26:47 +0300

    456

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,29 +8,16 @@ const Header = () => {
         <div ckassName="header__all">
           <img src={logo} alt="Logo" />
           <div className="header__nav">
-            <a href="Home" className="header__link">
-              Головна
-            </a>
-            <a href="About" className="header__link">
-              Магазин
-            </a>
+            <a href="Home" className="header__link"></a>
+            <a href="About" className="header__link"></a>
             <a href="Our Product" className="header__link">
               Блог
             </a>
-            <a href="Contact" className="header__link">
-              Пасіка
-            </a>
-            <a href="Contact" className="header__link">
-              Оплата і доставка
-            </a>
-            <a href="Contact" className="header__link">
-              Контакти
-            </a>
+            <a href="Contact" className="header__link"></a>
+            <a href="Contact" className="header__link"></a>
+            <a href="Contact" className="header__link"></a>
           </div>
-          <div>
-            <button className="header__search-btn">🔍</button>
-            <button className="header__cart">👜 0 Items Added</button>
-          </div>
+          <div></div>
         </div>
       </div>
     </header>
