50ab4d80bd6339709cf169c216aa1790f13ed1f9
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-10-22 22:08:19 +0300

    delete img

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,10 @@
 import "./header.scss";
-import logo from "../assets/img/honey.png";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
         <div ckassName="header__all">
-          <img src={logo} alt="Logo" />
           <div className="header__nav">
             <a href="Home" className="header__link"></a>
             <a href="About" className="header__link"></a>
