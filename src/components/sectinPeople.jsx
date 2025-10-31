e913d2a8265d046c99035554061f62d49b342ce9
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-10-31 22:41:54 +0200

    component

--- src/components/sectinPeople.jsx
+++ src/components/sectinPeople.jsx
@@ -1,20 +1,80 @@
-const Section = () => {
+import { NavLink } from "react-router-dom";
+import {
+  LayoutGrid,
+  MessageSquare,
+  ClipboardList,
+  Users,
+  Settings,
+  Bell,
+  ChevronDown,
+} from "lucide-react";
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  const menuItems = [
+    { name: "Dashboard", path: "/", icon: <LayoutGrid size={18} /> },
+    { name: "Messages", path: "/messages", icon: <MessageSquare size={18} /> },
+    { name: "My Tasks", path: "/tasks", icon: <ClipboardList size={18} /> },
+    { name: "Clients", path: "/clients", icon: <Users size={18} /> },
+  ];
+
+  const settingsItems = [
+    { name: "Main Settings", path: "/settings", icon: <Settings size={18} /> },
+    { name: "Notifications", path: "/notifications", icon: <Bell size={18} /> },
+  ];
+
   return (
-    <section className="section">
-      <div className="section__container">
-        <div className="section__all">
-          <div className="section__nav">
-            <a href="Home" className="section__link"></a>
-            <a href="About" className="section__link"></a>
-            <a href="Our Product" className="header__link">
-              People
-            </a>
+    <aside className="sidebar">
+      <div className="sidebar__top">
+        <p className="sidebar__title">Dashboard</p>
+
+        <div className="sidebar__user">
+          <img
+            src="https://i.pravatar.cc/100?img=3"
+            alt="user"
+            className="sidebar__avatar"
+          />
+          <div className="sidebar__user-info">
+            <span className="sidebar__name">Aaron Young</span>
+            <ChevronDown size={16} className="sidebar__chevron" />
           </div>
-          <div></div>
         </div>
+
+        <nav className="sidebar__menu">
+          {menuItems.map((item) => (
+            <NavLink
+              key={item.path}
+              to={item.path}
+              className={({ isActive }) =>
+                isActive ? "menu__item active" : "menu__item"
+              }
+            >
+              {item.icon}
+              <span>{item.name}</span>
+            </NavLink>
+          ))}
+        </nav>
       </div>
-    </section>
+
+      <div className="sidebar__bottom">
+        <p className="sidebar__subtitle">Settings</p>
+        <nav className="sidebar__menu">
+          {settingsItems.map((item) => (
+            <NavLink
+              key={item.path}
+              to={item.path}
+              className={({ isActive }) =>
+                isActive ? "menu__item active" : "menu__item"
+              }
+            >
+              {item.icon}
+              <span>{item.name}</span>
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+    </aside>
   );
 };
 
-export default Section;
+export default Sidebar;
