2369fa3a24f79e64fde519c179953c566f1973e9
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-07 22:50:29 +0200

    section new

--- src/components/sectionMenu.jsx
+++ src/components/sectionMenu.jsx
@@ -1,7 +1,15 @@
 import "./sectionMenu.scss";
 
 const SectionMenu = () => {
-  return <div className="sectionMenu">Mesaage</div>;
+  return (
+    <section className="sectionMenu">
+      <h2 className="sectionMenu__title">Explore Our Features</h2>
+      <p className="sectionMenu__description">
+        Discover powerful tools and intuitive design to elevate your workflow.
+      </p>
+      <button className="sectionMenu__button">Get Started</button>
+    </section>
+  );
 };
 
 export default SectionMenu;
