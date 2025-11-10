c9d309456b94a970c5d07c2075823639c9e45239
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-10 23:20:32 +0200

    add section

--- src/components/sectionMenu.jsx
+++ src/components/sectionMenu.jsx
@@ -3,11 +3,16 @@ import "./sectionMenu.scss";
 const SectionMenu = () => {
   return (
     <section className="sectionMenu">
-      <h2 className="sectionMenu__title">Explore Our Features</h2>
-      <p className="sectionMenu__description">
-        Discover powerful tools and intuitive design to elevate your workflow.
-      </p>
+      <h2 className="sectionMenu__title">Arong Young</h2>
+      <p className="sectionMenu__description">Dahboard</p>
+      <p className="sectionMenu__description">Message</p>
+      <p className="sectionMenu__description">My Tasks</p>
+      <p className="sectionMenu__description">Klients</p>
       <button className="sectionMenu__button">Get Started</button>
+
+      <p className="sectionMenu__description">Settings</p>
+      <p className="sectionMenu__description">Help & Support</p>
+      <p className="sectionMenu__description">Logout</p>
     </section>
   );
 };
