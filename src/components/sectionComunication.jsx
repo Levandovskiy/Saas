e1b048840021c8546649c17f4e4f7b665b1e1678
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-13 22:46:34 +0200

    wea

---
+++ src/components/sectionComunication.jsx
@@ -0,0 +1,24 @@
+import "./sectionMenu.scss";
+
+const SectionMenu = () => {
+  return (
+    <section className="sectionMenu">
+      <h2 className="sectionMenu__title">Arong Young</h2>
+      <p className="sectionMenu__description">Dahboard</p>
+      <p className="sectionMenu__description">Message</p>
+      <p className="sectionMenu__description">My Tasks</p>
+      <p className="sectionMenu__description">Klients</p>
+      <button className="sectionMenu__button">Get Started</button>
+
+      <p className="sectionMenu__description">Settings</p>
+      <p className="sectionMenu__description">Help & Support</p>
+      <p className="sectionMenu__description">Logout</p>
+
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+    </section>
+  );
+};
+
+export default SectionMenu;
