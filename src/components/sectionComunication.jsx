c349e2bfe3e30f416fe7df37264d0612daad956a
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-18 23:26:39 +0200

    99

--- src/components/sectionComunication.jsx
+++ src/components/sectionComunication.jsx
@@ -21,6 +21,42 @@ const SectionMenu = () => {
       <p className="sectionMenu__description"></p>
       <p className="sectionMenu__description"></p>
       <p className="sectionMenu__description"></p>
+
+      <p className="sectionMenu__description">Dahboard</p>
+      <p className="sectionMenu__description">Message</p>
+      <p className="sectionMenu__description">My Tasks</p>
+      <p className="sectionMenu__description">Klients</p>
+      <button className="sectionMenu__button">Get Started</button>
+
+      <p className="sectionMenu__description">Settings</p>
+      <p className="sectionMenu__description">Help & Support</p>
+      <p className="sectionMenu__description">Logout</p>
+
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+
+      <p className="sectionMenu__description">Dahboard</p>
+      <p className="sectionMenu__description">Message</p>
+      <p className="sectionMenu__description">My Tasks</p>
+      <p className="sectionMenu__description">Klients</p>
+      <button className="sectionMenu__button">Get Started</button>
+
+      <p className="sectionMenu__description">Settings</p>
+      <p className="sectionMenu__description">Help & Support</p>
+      <p className="sectionMenu__description">Logout</p>
+
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
+      <p className="sectionMenu__description"></p>
     </section>
   );
 };
