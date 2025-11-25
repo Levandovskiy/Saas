273e311fdcee1cf4cbc3de2b2d052fd5de36bde5
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-11-25 23:03:27 +0200

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import SectionMenu from "./components/sectionMenu";
 import Dashboard from "./components/dashboard";
+import CalendarActivity from "./calendar";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <SectionMenu />
       <Dashboard />
+      <CalendarActivity />
     </>
   );
 }
