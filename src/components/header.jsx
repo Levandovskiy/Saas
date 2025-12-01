567ceabec6f3c042cf493e53259469eaa726cc62
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-12-01 23:47:22 +0200

    nee

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,7 @@
 import "./header.scss";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="header__container">
-        <div className="header__all">
-          <div className="header__nav"></div>
-          <div></div>
-        </div>
-      </div>
-    </header>
-  );
+  return <header className="header"></header>;
 };
 
 export default Header;
